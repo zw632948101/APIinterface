5e24cd7eba2961302929992092a245341ca7de65
yaxin.guan@worldfarm.com <yaxin.guan@worldfarm.com>
2020-07-03 11:52:41 +0800

    添加fake文件

---
+++ testcase/flowerChaser/bee/NectarSourcePoint.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+from interfaces.flowerChaser.BeeAction import BeeAction
+from tools.Config import Log
+from testCase.FakeLocation import FakeLocation
+import random
+import json
+from faker import Faker
+import datetime, time
+import re
+from tools.Tool import Tool
