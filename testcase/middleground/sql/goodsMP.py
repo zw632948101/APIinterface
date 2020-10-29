8b667c6cdf20e3ef6680c47de9e56318d1e1b35a
chao.he <chao.he@worldfarm.com>
2020-10-29 11:17:20 +0800

    2020/10/29 edit by he.chao

--- testcase/middleground/sql/goodsMP.py
+++ testcase/middleground/sql/goodsMP.py
@@ -25,3 +25,22 @@ class mp_label(DataBaseOperate):
         """
         sql = "SELECT tl.id,tl.name,tl.type,tl.creator_id FROM `mp-product`.t_label tl WHERE tl.name = '%s' AND  tl.type = '%s';" % (label_name, label_type)
         return self.operate_db(sql=sql)
+    def query_mp_section_info(self,section_prefix=None,section_bizId=None):
+        """
+        查询商品属性
+        ：param section_prefix
+        ：param section_bizId
+        ：param section_num
+        """
+        if section_prefix and section_bizId:
+            sql = "select * from `mp-product`.t_section_no where biz_id={0} and prefix='{1}';"\
+                .format(section_bizId,section_prefix)
+        else:
+            sql = "select * from `mp-product`.t_section_no order by id desc limit 1;"
+        return self.operate_db(sql=sql)
+
+
+if __name__ == '__main__':
+    sql = mp_label()
+    a = sql.query_mp_section_info("T",1)
+    print(a)
