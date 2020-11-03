513605ce9531dc36e2ab1b960192a997cb825859
hui.liu <hui.liu@worldfarm.com>
2020-11-03 16:59:06 +0800

    inventory.py

--- testcase/middleground/goodsMiddleground/inventory.py
+++ testcase/middleground/goodsMiddleground/inventory.py
@@ -26,8 +26,7 @@ class inventory_data:
         {"title":"修改库存-为空","data":{"skuCode":"T0101010001","quantity":None},"expected":"ERROR"}
     ]
     inventory_list = [
-        {"title":"修改库存-为空","data":{"pn":"","ps":"","skuCode":"","skuName":""},"expected":"ERROR"},
-        {}
+        {"title":"修改库存-为空","data":{"pn":1,"ps":5,"skuCode":"T0101010001","skuName":"追"},"expected":"OK"}
     ]
 @ddt
 class inventory(unittest.TestCase):
@@ -40,23 +39,30 @@ class inventory(unittest.TestCase):
         self.api.set_user(mobile=15388126072)
         self.db = mp_label()
 
-    @data(*inventory_data().inventory_add)
-    def test_admin_inventory_add(self,case):
-        skuId_ = case['data']['skuId_']
-        skuCode_ = case['data']['skuCode_']
-        quantity_ = case['data']['quantity_']
-        resp = self.api._admin_inventory_add(skuId_=skuId_, skuCode_=skuCode_, quantity_=quantity_)
+    # @data(*inventory_data().inventory_add)
+    # def test_admin_inventory_add(self,case):
+    #     skuId_ = case['data']['skuId_']
+    #     skuCode_ = case['data']['skuCode_']
+    #     quantity_ = case['data']['quantity_']
+    #     resp = self.api._admin_inventory_add(skuId_=skuId_, skuCode_=skuCode_, quantity_=quantity_)
+    #     self.assertEqual(case['expected'],resp.get('status'))
+    #
+    @data(*inventory_data().inventory_list)
+    def test_admin_inventory_page_list(self,case):
+        pn = case['data']['pn']
+        ps = case['data']['ps']
+        skuCode = case['data']['skuCode']
+        skuName = case['data']['skuName']
+
+        resp = self.api._admin_inventory_page_list(pn_=pn, ps_=ps, skuCode_=skuCode, skuName_=skuName)
         self.assertEqual(case['expected'],resp.get('status'))
 
-    def test_admin_inventory_page_list(self):
-        pass
-
-    @data(*inventory_data().inventory_update)
-    def test_admin_inventory_update(self,case):
-        skuCode_ = case['data']['skuCode']
-        quantity_ = case['data']['quantity']
-        resp = self.api._admin_inventory_update(skuCode_=skuCode_, quantity_=quantity_)
-        self.assertEqual(case['expected'],resp.get('status'))
+    # @data(*inventory_data().inventory_update)
+    # def test_admin_inventory_update(self,case):
+    #     skuCode_ = case['data']['skuCode']
+    #     quantity_ = case['data']['quantity']
+    #     resp = self.api._admin_inventory_update(skuCode_=skuCode_, quantity_=quantity_)
+    #     self.assertEqual(case['expected'],resp.get('status'))
 
 if __name__ == '__main__':
     unittest.main()
