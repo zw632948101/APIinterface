ee15399bc6e927e4806851c78d11b97093d9809a
张伟 <wei.zhang@worldfarm.com>
2020-11-03 15:55:25 +0800

    添加小程序case文件

---
+++ testcase/shop/applet/evaluate.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# @Time:2020/11/3 15:54
+# @Author: wei.zhang
+# @File : evaluate.py
+# @Software: PyCharm
+
+class A(object):
+    def __init__(self):
+        pass
---
+++ testcase/shop/applet/shoppingCart.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# @Time:2020/11/3 15:50
+# @Author: wei.zhang
+# @File : shoppingCart.py
+# @Software: PyCharm
+
+class A(object):
+    def __init__(self):
+        pass
