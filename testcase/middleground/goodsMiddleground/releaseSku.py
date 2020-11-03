1b744f52865fdefe62695d83f83ae59c2cafde5a
hui.liu <hui.liu@worldfarm.com>
2020-11-03 14:39:36 +0800

    fix releasesku file

---
+++ testcase/middleground/goodsMiddleground/releaseSku.py
@@ -0,0 +1,14 @@
+from utils.log import log
+import time
+import random
+import warnings
+from interfaces.middleground.ProductAction import ProductAction
+from testcase.middleground.sql.brandMP import mp_label
+from utils import runlevel, timestamp
+from ddt import data, unpack, ddt
+from faker import Faker
+import unittest
+
+
+class releaseManage():
+    pass
