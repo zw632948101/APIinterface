6eec06c8f6141cf2c3e7908bd8a87f1322ca98fe
张伟 <wei.zhang@worldfarm.com>
2021-01-06 17:25:55 +0800

    更新updateaction文件

--- utils/generatedInterface/UpdateAction.py
+++ utils/generatedInterface/UpdateAction.py
@@ -219,12 +219,8 @@ class UpdateAction(object):
 
 
 if __name__ == '__main__':
-    # update_action = UpdateAction()
-    # update_action.add_latest_action()
+    update_action = UpdateAction()
+    update_action.add_latest_action()
     # update_action.add_latest_yaml()
     # update_action.add_path_only_txt()
-    # update_action.get_edit_api()
-    from utils.checkApiChanges.model.dbEngine import DBEngine
-    db = DBEngine()
-    # db.db_create()
-    db.update_migrate()
+    update_action.get_edit_api()
