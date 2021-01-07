72c4a584517975d0e10d89f93de90f80ffef45f3
张伟 <wei.zhang@worldfarm.com>
2021-01-07 13:38:21 +0800

    增加wms接口测试，员工模块

--- testcase/middleground/WMS/test_case/test_employee.py
+++ testcase/middleground/WMS/test_case/test_employee.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# @Time:2021/1/6 17:54
+# @Time:2021/1/7 12:00
 # @Author: wei.zhang
-# @File : test_company.py
+# @File : test_employee.py
 # @Software: PyCharm
 
 from interfaces.middleground.Wms_apiAction import wms_apiAction
@@ -15,8 +15,10 @@ import unittest
 """
 中台接口测试- 公司/运营主体
 """
+
+
 @ddt
-class Company(unittest.TestCase):
+class Employee(unittest.TestCase):
     def setUp(self) -> None:
         """
         测试前数据准备
@@ -29,15 +31,10 @@ class Company(unittest.TestCase):
 
     def tearDown(self) -> None: pass
 
-    @unittest.skipIf(runlevel(1), "admin-公司/运营主体 列表")
-    def test_admin_company_list(self):
+    def test_admin_employee_list(self):
         """
-        admin-公司/运营主体 列表
+        admin-员工-列表
         :return:
         """
-        resp = self.api._admin_company_list()
+        resp = self.api._admin_employee_list()
         self.assertEqual(resp.get('status'), 'OK')
-        content = resp.get('content')
-        subjectinfo = self.db.query_wms_subject_info()
-        for ct, sj in zip(content, subjectinfo):
-            self.assertDictEqual(ct, sj)
