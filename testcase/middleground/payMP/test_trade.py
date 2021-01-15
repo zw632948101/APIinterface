600151b6abd8dbd403f9637d5d79606032fbc368
张伟 <wei.zhang@worldfarm.com>
2021-01-15 15:23:31 +0800

    增加中台接口，支付结算case

--- testcase/middleground/payMP/test_trade.py
+++ testcase/middleground/payMP/test_trade.py
@@ -34,17 +34,17 @@ class TradeTest(unittest.TestCase):
         当面付 - 付款码支付
         :return:
         """
-        authCode = '134617647963252055'
-        bodyId = '0000001'
-        disregardaAmount = 1
-        due = 0
-        mchCreateIp = '172.12.13.15'
-        mchNo = '2088041261907815'
-        outOrderNo = '25358884655467'
-        regardaAmount = 0
-        source = 'POS'
-        subject = '椴树蜜'
-        totalAmount = 1
+        authCode = '283685125040405890'  # 付款码id
+        bodyId = '0000001'  # 渠道id
+        disregardaAmount = 1  # 优惠后金额
+        due = 0  # 优惠金额
+        mchCreateIp = '172.12.13.15'  # 商户地址
+        mchNo = '2088041261907815'  # 商户号
+        outOrderNo = '25358884655469'  # 外部订单号
+        regardaAmount = 0  #
+        source = 'POS'  # 来源渠道
+        subject = '椴树蜜'  # 商品名称
+        totalAmount = 1  # 实收金额
         resp = self.api._open_api_trade_tradePay(authCode_=authCode, bodyId_=bodyId,
                                                  disregardaAmount_=disregardaAmount, due_=due,
                                                  mchCreateIp_=mchCreateIp, mchNo_=mchNo,
